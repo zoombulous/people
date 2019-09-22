f07717e31028496fbbf6dec40ac106f4975e084e
Donnie Cameron <macnod@gmail.com>
2019-09-22 12:07:03 -0700

    Configured PeopleList to load JSON file from localhost.

--- src/App.vue
+++ src/App.vue
@@ -1,71 +1,35 @@
 <template>
-  <div id="app">
-    <h1>Todos</h1>
-    <input type="text" v-model="personName" @keyup.enter="addPerson">
-    <ul>
-      <li v-for="person of people" :key="person.id">{{person.name}}</li>
-    </ul>
+  <div id="app" class="small-container">
+    <h1>People List</h1>
+    <people-list :people="people"/>
   </div>
 </template>
 
 <script>
-    import axios from 'axios';
-const baseURL = "http://localhost:3000/todos"
+
+import PeopleList from "./components/PeopleList";
+import people_json from "./json/people.json";
+
 export default {
-  name: 'app',
-    data() {
-	return {
-	    people: []
-	    first: ''
-	    last: ''
-	    age: ''
-	    weight: ''
-	    height: ''
-	};
-    },
-    async created() {
-	try {
-	    const res = await axios.get(baseURL);
-	    this.people = res.data;
-	} catch(e) {
-	    console.error(e);
-	}
-    },
-    methods: {
-	async addPerson() {
-	    const res = await axios.post(baseURL, { first: this.first
-						    last: this.last
-						    age: this.age
-						    weight: this.weight
-						    height: this.height
-						  })
-	    this.people = [...this.people, res.data];
-	    this.first = "";
-	    this.last = "";
-	    this.age = "";
-	    this.weight = "";
-	    this.height = '';
-	}
+  name: "app",
+  components: {
+    PeopleList
+  },
+  data () {
+    return {
+      people: people_json.people
     }
+  }
 };
-						    
 </script>
-    
-    <style>
+
+<style>
 #app {
-    font-family: 'Avenir', Helvetica, Arial, sans-serif;
-	-webkit-font-smoothing: antialiased;
-	-moz-osx-font-smoothing: grayscale;
-    text-align: center;
-    color: #2c3e50;
-    margin-top: 60px;
-}
-ul {
-    margin: 0px;
-    padding: 0px;
-}
-li {
-    list-style: none;
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  color: #2c3e50;
+  margin-top: 60px;
+  width: 80%;
 }
 </style>
-    
