3d2d9f918f2274457d2dfa299afbffe3fa46969f
Donnie Cameron <macnod@gmail.com>
2019-09-26 22:22:24 -0700

    Added some files that were missing from the branch.

---
+++ src/components/PeopleList.vue
@@ -0,0 +1,25 @@
+<template>
+  <div id="people-list">
+    <ul>
+      <li v-for="person in people" :key="person.id">
+          {{ person.first }} {{person.last}}
+      </li>
+      <li>Person 2</li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "people-list",
+  props: {
+    people: Array
+  }
+};
+</script>
+
+<style scoped>
+h1 {
+  margin: 40px 0 0;
+}
+</style>
