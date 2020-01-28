fdf3aece8cf78bb638dc08b038f3a101397fa356
zoombulous <44047136+zoombulous@users.noreply.github.com>
2020-01-28 09:16:20 -0800

    working!
    Need to add in watcher, but vue is throwing errors when defining variable for the contains function

--- src/App.vue
+++ src/App.vue
@@ -1,35 +1,29 @@
 <template>
-    <div id="app">
-    <input v-model="startsWith" placeholder="starts with"/>
-    <input v-model="contains" placeholder="contains these letters"/>
-    <button id="btn" class="" v-on:click="findNames">Go</button>
-    <p>Result:</p>
-    <ul>
-      <li v-for="name in names">{{ name }}</li>
-    </ul>
-  </div>  
+  <div id="app">
+    <pre v-if="info">
+      {{ info }}
+    </pre>
+    <div v-else>loading...</div>
+  </div>
 </template>
 
 <script>
+import axios from "axios";
 
-
-  new Vue({
-    el: '#app',
-    data: {
+export default {
+  name: "App",
+  data() {
+    return {
+        info: null,
         contains: '',
         startsWith: '',
         names: [],
         last_event: 0,
         cancel: 0
-    },
-    watch: {
-        contains() {
-            t = (new Date()).getTime();
-            if(t - this.last_event > 500)
-                this.findNames();
-            this.last_event = t;
-        }
-    },
+    };
+  },
+    
+
     methods: {
         findNames() {
             axios.get(
@@ -39,12 +33,10 @@
                     + '&contains-letters=' + this.contains
                     + '&starts-with=' + this.startsWith
             ).then((response) => {
-                    this.names = response.data.results.map(item => item.name);
-                });
+                this.names = response.data.results.map(item => item.name);
+            });
         }
-    }
-});
+  }
+};
 </script>
 
-<style>
-</style>
