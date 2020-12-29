d45867e9974199109c3ed1da64366485412cfc27
zoombulous <44047136+zoombulous@users.noreply.github.com>
2020-12-29 11:13:07 -0800

    made API address bar start with HTTPS

--- src/App.vue
+++ src/App.vue
@@ -229,7 +229,7 @@ export default {
             function () {
                 axios
                     .get(
-                    'http://names.sinistercode.com:4242/api/names?'
+                    'https://names.sinistercode.com:4242/api/names?'
                         + 'format=json'
                         + '&sort=' + this.sortBy
                         + '&contains-letters=' + this.contains
