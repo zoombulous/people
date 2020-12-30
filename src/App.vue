8007105cb6279e4cb769cf19dfcb955bcfd8db5a
zoombulous <44047136+zoombulous@users.noreply.github.com>
2020-12-30 09:17:08 -0800

    changed https back to http

--- src/App.vue
+++ src/App.vue
@@ -229,7 +229,7 @@ export default {
             function () {
                 axios
                     .get(
-                    'https://names.sinistercode.com:4242/api/names?'
+                    'http://names.sinistercode.com:4242/api/names?'
                         + 'format=json'
                         + '&sort=' + this.sortBy
                         + '&contains-letters=' + this.contains
